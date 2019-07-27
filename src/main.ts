8a7a4552739e30bd0a395f736e1e926d07f5bc4f
Gustav Kofoed Clausen <gustav@gustavclausen.com>
2019-07-27 13:59:50 +0200

    Cleanup module interface + include models

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,7 @@
 import APIFetcher from './fetcher/api-fetcher';
-import { RequestError, ResponseErrorType, ParseError } from './lib/errors';
-import {
-    GraphQLRequest,
-    GraphQLPagedRequest,
-    GraphQLFragment,
-    GraphQLObjectField,
-    PageInfo
-} from './fetcher/graphql/utils';
 
-export {
-    APIFetcher,
-    RequestError,
-    ResponseErrorType,
-    ParseError,
-    GraphQLRequest,
-    GraphQLPagedRequest,
-    GraphQLFragment,
-    GraphQLObjectField,
-    PageInfo
-};
+export { APIFetcher };
+export * from './lib/errors';
+export * from './lib/date-utils';
+export * from './fetcher/graphql/utils';
+export * from './models';
